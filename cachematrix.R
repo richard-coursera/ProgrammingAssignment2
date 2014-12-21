2071adfc5d043385117400ba113507f01d3f1e86
richard-coursera <2305897460@qq.com>
2014-12-21 21:07:51 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,3 @@
-##
 ## These two functions make a matrix inverse and cached.
 ##
 ## makeCacheMatrix creates the four operations on a matrix. 
@@ -6,7 +5,6 @@
 ## 2. Get the matrix
 ## 3. Set up the inverse matrix
 ## 4. Get the inverse matrix
-## 
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
@@ -22,11 +20,9 @@ makeCacheMatrix <- function(x = matrix()) {
              getmatrix = getmatrix)
 }
 
-##
 ## cacheSolve function checks whether the inverse matrix is cached.
 ## If inverse matrix is cached, it will be returned.
 ## Otherwise, cacheSolve makes the matrix inverse, and inverse matrix is cached and returned.
-## 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
